207513cff2683efac25504edfc399dbf9e798e81
Ruben Ortiz <rubenortiz.gv@gmail.com>
2020-12-14 16:53:55 +0100

    Update maxCols logic.

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -5,27 +5,35 @@ import { Button } from 'components/Button';
 export const Table = ({
   model,
   rows,
+  maxCols = 5,
 }: {
   model: Model;
   rows: Array<{ [index: string]: undefined | string }>;
+  maxCols?: number;
 }) => {
-  const MAX_AMOUNT_OF_PROPS = 5;
   return (
     <table className="min-w-full divide-y divide-gray-200">
       <thead className="bg-gray-50">
         <tr>
           {model.properties.map(
-            (property, key) =>
-              property.type === PropertyType.String &&
-              key <= MAX_AMOUNT_OF_PROPS && (
-                <th
-                  key={key}
-                  scope="col"
-                  className="px-6 py-3 text-left text-gray-700 font-bold"
-                >
-                  {ucfirst(property.name)}
-                </th>
-              ),
+            (() => {
+              let counter = 0;
+              return ({ type, name }: { type: PropertyType; name: string }) => {
+                counter += 1;
+                return (
+                  type === PropertyType.String &&
+                  counter <= maxCols && (
+                    <th
+                      key={counter}
+                      scope="col"
+                      className="px-6 py-3 text-left text-gray-700 font-bold"
+                    >
+                      {ucfirst(name)}
+                    </th>
+                  )
+                );
+              };
+            })(),
           )}
           <th
             scope="col"
@@ -39,15 +47,28 @@ export const Table = ({
         {rows.map((row, rowKey) => (
           <tr key={rowKey}>
             {model.properties.map(
-              (property, key) =>
-                property.type === PropertyType.String &&
-                key <= MAX_AMOUNT_OF_PROPS && (
-                  <td className="px-6 py-4 whitespace-nowrap" key={key}>
-                    <div className="flex items-center">
-                      {row[property.name] || '-'}
-                    </div>
-                  </td>
-                ),
+              (() => {
+                let counter = 0;
+                return ({
+                  type,
+                  name,
+                }: {
+                  type: PropertyType;
+                  name: string;
+                }) => {
+                  counter += 1;
+                  return (
+                    type === PropertyType.String &&
+                    counter <= maxCols && (
+                      <td className="px-6 py-4 whitespace-nowrap" key={counter}>
+                        <div className="flex items-center">
+                          {row[name] || '-'}
+                        </div>
+                      </td>
+                    )
+                  );
+                };
+              })(),
             )}
             <td className="px-6 py-4">
               <Button>View</Button>
