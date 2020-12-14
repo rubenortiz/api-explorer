85ef9e9266f777adac3bb30f00ceaca482faff10
Ruben Ortiz <rubenortiz.gv@gmail.com>
2020-12-14 14:38:24 +0100

    Navigation

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react';
 import cn from 'classnames';
+import { Navigation } from 'components/Navigation';
 
 type Layout = React.FC<{
   fullWidth?: boolean;
@@ -8,13 +9,16 @@ type Layout = React.FC<{
 
 export const Layout: Layout = ({ fullWidth = false, children }) => {
   return (
-    <div
-      className={cn({
-        'py-10 px-6 lg:px-8 xl:px-0': true,
-        'container mx-auto': !fullWidth,
-      })}
-    >
-      {children}
+    <div className="min-h-screen flex flex-row bg-gray-100">
+      <Navigation />
+      <div
+        className={cn({
+          'py-10 px-6 lg:px-8 xl:px-0': true,
+          'container mx-auto': !fullWidth,
+        })}
+      >
+        {children}
+      </div>
     </div>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,4 @@
 import Head from 'next/head';
-import Link from 'next/link';
-import models from 'models';
 import { Layout } from 'components/Layout';
 
 export default function Home() {
@@ -13,16 +11,6 @@ export default function Home() {
         <h1 className="mb-5 text-3xl font-extrabold text-gray-900">
           Rick & Morty
         </h1>
-        {models.map((model, key) => (
-          <h2
-            key={key}
-            className="text-lg mb-3 leading-6 font-medium text-gray-900"
-          >
-            <Link href="/[model]" as={`/${model.name.toLowerCase()}`}>
-              <a>{model.name}</a>
-            </Link>
-          </h2>
-        ))}
       </Layout>
     </div>
   );
